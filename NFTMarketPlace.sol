c1c743ab207f086a4b9647d67d3dbf40c303ce61
Raayudu <111636763+Raayudu@users.noreply.github.com>
2023-04-13 10:45:52 +0530

    updated by adding comments
    comments added to all functions

--- NFTMarketPlace.sol
+++ NFTMarketPlace.sol
@@ -2,26 +2,26 @@
 
 pragma solidity^0.8.19;
 
-import "@openzeppelin/contracts/utils/Counters.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "hardhat/console.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";    //import counters from utils
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";  //import ERC721URIStorage
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";   //import ERC721
+import "hardhat/console.sol";   //import console from harhat
 
-    contract NFTMarketplace is ERC721URIStorage {
+    contract NFTMarketplace is ERC721URIStorage {   //contract inheriting ERC721URIStorage to NFTMarketplace
             using Counters for Counters.Counter;
             Counters.Counter private _tokenIds;
             Counters.Counter private _itemsSold;
-            uint256 listingPrice = 0.00025 ether;
-            address payable owner;
+            uint256 listingPrice = 0.00025 ether;   //price of ether
+            address payable owner;  //address of owner
             mapping(uint256 => MarketItem) private idToMarketItem;
-        struct MarketItem {
+        struct MarketItem { //item market details
             uint256 tokenId;
             address payable seller;
             address payable owner;
             uint256 price;
             bool sold;
         }
-        event MarketItemCreated (
+        event MarketItemCreated (   //event of market
             uint256 indexed tokenId,
             address seller,
             address owner,
@@ -31,15 +31,15 @@ import "hardhat/console.sol";
         constructor() ERC721("Metaverse Tokens", "METT") {
             owner = payable(msg.sender);
         }
-        function updateListingPrice(uint _listingPrice) public payable {
+        function updateListingPrice(uint _listingPrice) public payable {    //function used to update listing price
             require(owner == msg.sender, "Only marketplace owner can update listing price");
             listingPrice = _listingPrice;
            
         }
-        function getListingPrice() public view returns (uint256) {
+        function getListingPrice() public view returns (uint256) {  //get item lising price
             return listingPrice;
         }
-        function createToken (string memory tokenURI, uint256 price) public payable returns (uint) {
+        function createToken (string memory tokenURI, uint256 price) public payable returns (uint) {    // to create token
             _tokenIds.increment ();
             uint256 newTokenId = _tokenIds.current ();
             _mint(msg.sender, newTokenId);
@@ -47,14 +47,14 @@ import "hardhat/console.sol";
             createMarketItem(newTokenId, price);
             return newTokenId;
         }
-        function createMarketItem(uint256 tokenId, uint256 price) private {
+        function createMarketItem(uint256 tokenId, uint256 price) private {     // to create Market Item
             require(price > 0, "Price must be at least 1wei");
             require(msg.value == listingPrice, "Price must be equal to listing price");
             idToMarketItem[tokenId] = MarketItem(tokenId,payable(msg.sender),payable(address(this)),price,false);
             _transfer(msg.sender, address(this), tokenId);
             emit MarketItemCreated(tokenId,msg.sender,address(this),price,false);
         } 
-        function reselToken(uint256 tokenId, uint256 price) public payable {
+        function reselToken(uint256 tokenId, uint256 price) public payable {    // to sell token again which is we bought already
             require(idToMarketItem[tokenId].owner == msg.sender, "only item owner can perform this operation");
             require(msg.value == listingPrice, "Price must be equal to listing price");
             idToMarketItem[tokenId].sold = false;
@@ -64,7 +64,7 @@ import "hardhat/console.sol";
             _itemsSold.decrement();
             _transfer(msg.sender, address(this), tokenId);
         }
-        function createMarketSale (uint256 tokenId) public payable {
+        function createMarketSale (uint256 tokenId) public payable {       // to create Market Sale for token
             uint price = idToMarketItem[tokenId].price;
             address payable creator = idToMarketItem[tokenId].seller;
             require(msg.value == price, "Please submit the asking price in order to complete the purchase");
@@ -76,7 +76,7 @@ import "hardhat/console.sol";
             payable(owner).transfer(listingPrice);
             payable(creator).transfer(msg.value);           
         }
-        function fetchMarketItems() public view returns (MarketItem[] memory) {
+        function fetchMarketItems() public view returns (MarketItem[] memory) {   //function to use to fetch items to market   
             uint itemCount = _tokenIds.current();
             uint unsoldItemCount = _tokenIds.current() - _itemsSold.current();
             uint currentIndex = 0;
@@ -91,7 +91,7 @@ import "hardhat/console.sol";
         }
         return items;
     }
-        function fetchMyNFTs() public view returns (MarketItem[] memory) {
+        function fetchMyNFTs() public view returns (MarketItem[] memory) {      //to add nft's
             uint totalItemCount = _tokenIds.current();
             uint itemCount = 0;
             uint currentIndex = 0;
@@ -112,7 +112,7 @@ import "hardhat/console.sol";
     }
         return items;
     }
-        function fetchItemsListed() public view returns (MarketItem[] memory) {
+        function fetchItemsListed() public view returns (MarketItem[] memory) {     // to fetch items to be listed in market
             uint totalItemCount = _tokenIds.current();
             uint itemCount = 0;
             uint currentIndex = 0;
